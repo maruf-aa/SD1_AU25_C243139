4cdc36ab2affec2d8d53839b3f2bac530df6c22d
AA_Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-09-14 22:38:26 +0600

    Update 3.java

--- Solve Problems/3.java
+++ Solve Problems/3.java
@@ -1,1 +1,20 @@
+//3. Check if a number is even or odd
 
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a % 2 == 0) 
+        {
+            System.out.println("Even");
+        } 
+        else 
+        {
+            System.out.println("Odd");
+        }
+    }
+
+}
