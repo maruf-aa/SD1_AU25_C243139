ef1a330876c8206c30945f3cba96906e2f89e725
AA_Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-09-14 22:37:32 +0600

    Update 2.java

--- Solve Problems/2.java
+++ Solve Problems/2.java
@@ -1,1 +1,14 @@
+/* 2. Take user’s name and greet them. Example, Input: “Your Name” and Output: Hello, “Your
+Name”! */
+
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name);
+    }
+
+}
 
