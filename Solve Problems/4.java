e196de7ae66c160e27ed0b39b26581db06de2ab7
AA_Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-09-14 22:39:06 +0600

    Update 4.java

--- Solve Problems/4.java
+++ Solve Problems/4.java
@@ -1,1 +1,25 @@
+//4. Find the maximum of 3 numbers: Take 3 numbers from user input, print the largest.
 
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a > b && a > c) 
+        {
+            System.out.println("Largest is " + a);
+        } 
+        else if(b > a && b > c)
+        {
+            System.out.println("Largest is " + b);
+        }
+        else
+        {
+            System.out.println("Largest is " + c);
+        }
+    }
+
+}
