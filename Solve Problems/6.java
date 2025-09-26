d0dbba6aa4164cf5b9daeb53cf3afbbf42fdc71e
AA_Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-09-27 00:57:28 +0600

    Update 6.java

--- Solve Problems/6.java
+++ Solve Problems/6.java
@@ -1,1 +1,18 @@
 //6. Print numbers from 1 to N, Take N from user input.
+
+import java.util.*;
+public class Firstclass {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
+
+
