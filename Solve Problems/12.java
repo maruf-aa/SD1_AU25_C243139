fa969c9928dd3c4e081e4fb2ebbcb8659087cca1
Abdullah Al Maruf <132283359+maruf-aa@users.noreply.github.com>
2025-10-25 20:58:51 +0600

    Create 12.java

---
+++ Solve Problems/12.java
@@ -0,0 +1,28 @@
+//Find the smallest number in an array
+
+import java.util.*;
+public class Firstclass {
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int sz = sc.nextInt();
+
+        int marks[] = new int[sz];
+
+        for (int i = 0; i < sz; i++) 
+        {
+            marks[i] = sc.nextInt();
+        }
+
+        int min = marks[0];
+        for (int i = 0; i < sz; i++) 
+        {
+            if (marks[i] < min) 
+            {
+                min = marks[i];
+            }
+        }
+        System.out.println("Minimum marks: " + min);
+    }
+}
